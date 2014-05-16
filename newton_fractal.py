ec56b3471b81fd5d14e584c2a011723893537af5
Chris Laverdiere <cmlaverdiere@gmail.com>
2014-05-16 13:17:21 -0400

    Generalize newton fractal generator. Now works for supplied polynomial, with dynamic colors.

--- newton_fractal.py
+++ newton_fractal.py
@@ -5,23 +5,50 @@
 #   to c++ later for practice.
 
 import Image, ImageDraw, math
+from sympy.mpmath import *
 from calculus import newton
 from math import pi, sin
 
+# Some polynomials to play around with.
+ff = lambda z: z**3 - 1
+pf = [1, 0, 0, -1]
+fg = lambda z: z**3 - 2*z + 2
+pg = [1, 0, -2, 2]
+fh = lambda z: z**8 + 15*z**4 - 16
+ph = [1, 0, 0, 0, 15, 0, 0, 0, -16]
+
+# Choose polynomial / function to use for fractal.
+f = fg
+p = pg
+
+# Calculate real/complex roots of function using sympy.
+roots = []
+for r in polyroots(p):
+    roots.append(r)
+
+# Drawing constants
 dimx = dimy = 512
-shift = dimx/2
+shift = dimx / 2
 img = Image.new("RGB", (dimx, dimy))
 draw = ImageDraw.Draw(img)
 
-f = lambda z: z**3 - 1
-roots = [complex(1, 0), complex(-0.5, sin(2*pi/3)), complex(-0.5, -sin(2*pi/3))]
+# Accuracy constants
 max_iters = 15
 epsilon = .01
 bound = int(2 / epsilon)
 
-red = "rgb(255, 0, 0)"
-green = "rgb(0, 255, 0)"
-blue = "rgb(0, 0, 255)"
+# Color constants
+blue = (0, 0, 255)
+red = (255, 0, 0)
+green = (0, 255, 0)
+yellow = (255, 255, 0)
+magenta = (255, 0, 255)
+pink = (255, 128, 128)
+gray = (128, 128, 128)
+brown = (128, 0, 0)
+orange = (255, 128, 0)
+
+colors = [blue, red, green, yellow, magenta, pink, gray, brown, orange]
 
 for a in range(-dimx/2, dimx/2):
     for b in range(-dimy/2, dimy/2):
@@ -30,17 +57,17 @@ for a in range(-dimx/2, dimx/2):
         c, depth = newton.newton(f, max_iters, complex(a*epsilon, b*epsilon))
 
         # Apply shading based on depth of newton function.
-        color_depth_mod = 255 / (depth/2 + 1)
+        color_depth_mod = (depth/2 + 1)
 
         # Decide which color to draw.
-        if abs(c - roots[0]) < epsilon:
-            draw.point((a+shift, b+shift), fill="rgb(%d, %d, %d)" % (color_depth_mod, 0, 0))
-        elif abs(c - roots[1]) < epsilon:
-            draw.point((a+shift, b+shift), fill="rgb(%d, %d, %d)" % (0, color_depth_mod, 0))
-        elif abs(c - roots[2]) < epsilon:
-            draw.point((a+shift, b+shift), fill="rgb(%d, %d, %d)" % (0, 0, color_depth_mod))
-        else:
+        found = False
+        for i in range(len(roots)):
+            if abs(c - roots[i]) < epsilon:
+                dcolor = [c / color_depth_mod for c in colors[i]]
+                draw.point((a+shift, b+shift), tuple(dcolor))
+                found = True
+        if not found:
             draw.point((a+shift, b+shift), fill="rgb(255, 255, 0)")
 
 del draw
-img.save("results/newton_result3.png");
+img.save("results/newton_result5.png");
---
+++ test.py
@@ -0,0 +1,4 @@
+from sympy.mpmath import *
+
+for r in polyroots([1, 0, 0, -1]):
+    print r
